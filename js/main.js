7f5ad792d51e4f701e0dc1766a55a8286cd64c32
BacteriaQH <hoangphuc2k1vp@gmail.com>
2022-04-27 21:38:41 +0700

    add layout grade

--- js/main.js
+++ js/main.js
@@ -1,88 +1,97 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Sidebar Toggler
+  $(".sidebar-toggler").click(function () {
+    $(".sidebar, .content").toggleClass("open");
+    return false;
+  });
+  /* Upload File*/
+  $("#upload").change(function () {
+    const form = new FormData();
+    form.append("file", $(this)[0].files[0]);
+    $.ajax({
+      processData: false,
+      contentType: false,
+      type: "POST",
+      dataType: "JSON",
+      data: form,
+      url: "student/uploads/image",
+      success: function (results) {
+        if (results.error === false) {
+          $(".remove").remove();
+          $("#image_show").html(
+            '<a href="' +
+              results.url +
+              '" target="_blank" ><img src="' +
+              results.url +
+              '" height="200px" alt="' +
+              results.name +
+              '"></a>'
+          );
+          $("#thumb").val(results.url);
         } else {
-            $('.back-to-top').fadeOut('slow');
+          alert("Upload file không thành công");
         }
+      },
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
+    $(document).ready(function () {
+      $("#select")
+        .on("change", function () {
+          $(".input-show").hide();
+          $("#" + $(this).val()).fadeIn(700);
+        })
+        .change();
     });
-
-
-    // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
-        return false;
-    });
-    /* Upload File*/
-    $('#upload').change(function () {
-        const form = new FormData();
-        form.append('file', $(this)[0].files[0]);
-        $.ajax({
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            dataType: 'JSON',
-            data: form,
-            url: 'student/uploads/image',
-            success: function (results) {
-                if (results.error === false) {
-                    $('.remove').remove();
-                    $('#image_show').html('<a href="' + results.url + '" target="_blank" ><img src="' + results.url + '" height="200px" alt="' + results.name + '"></a>')
-                $('#thumb').val(results.url);
-                } else {
-                    alert('Upload file không thành công');
-                }
-            }
-        });
-        $('.selectpicker').selectpicker({
-            style: 'btn-default'
-        });
-
-        $('.selectpicker').on('change', function () {
-            $('#student_code').hide();
-            $('#class_code').hide();
-            $('#subject').hide();
-            $.each($(this).find('option:selected'), function (index, item) {
-                var selected = $(item).data('tokens');
-                    $("#" + selected).show();
-            });
-        });
-});
+  });
 })(jQuery);
 
+$(document).ready(function () {
+    $("#select")
+      .on("change", function () {
+        $(".input-show").hide();
+        $("#" + $(this).val()).fadeIn(700);
+      })
+      .change();
+  });
 function removeRow(id, url) {
-    if (confirm('Xoá mà không thể khôi phục. Bạn có chắc chắn không ?')) {
-        $.ajax({
-            type: 'DELETE',
-            datatype: 'JSON',
-            data: {id},
-            url: url,
-            success: function (result) {
-                if (result.error === false) {
-                    alert(result.message)
-                    location.reload();
-                } else {
-                    alert('Đã có lỗi xảy ra. Vui lòng thử lại');
-                }
-            }
-        })
-    }
+  if (confirm("Xoá mà không thể khôi phục. Bạn có chắc chắn không ?")) {
+    $.ajax({
+      type: "DELETE",
+      datatype: "JSON",
+      data: { id },
+      url: url,
+      success: function (result) {
+        if (result.error === false) {
+          alert(result.message);
+          location.reload();
+        } else {
+          alert("Đã có lỗi xảy ra. Vui lòng thử lại");
+        }
+      },
+    });
+  }
 }
